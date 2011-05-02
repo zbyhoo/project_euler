9e89daaa0788dde4a2e513634d23a4c6cbbaac37
zbyhoo <zbyhoo@gmail.com>
2011-05-02 21:41:09 +0200

    added description to problem 1

--- 001.rb
+++ 001.rb
@@ -1,5 +1,9 @@
 #!/bin/ruby
 
+# If we list all the natural numbers below 10 that are multiples of 3 or 5,
+# we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
 def number_ok(number)
 	return (number % 3 == 0 or number % 5 == 0)
 end
