222d1ecbef19ec5f271945d63f5f3cf7dcc12ce3
zbyhoo <zbyhoo@gmail.com>
2011-05-02 22:47:07 +0200

    problem 3 solved

---
+++ 003.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby
+
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
+
+def max_prime_factor(number, factor)
+	factor += 1 while number % factor != 0
+	if (number > factor)
+		return [factor, max_prime_factor(number / factor, factor)].max
+	end
+	return factor
+end
+
+puts max_prime_factor(600851475143, 2)
